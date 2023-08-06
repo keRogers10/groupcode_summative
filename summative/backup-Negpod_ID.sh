1a5f86485b5b2d0778f05f080c802b73e08e8725
keRogers10 <r.murara@alustudent.com>
2023-08-06 07:23:21 -0700

    this one is the money

--- summative/backup-Negpod_ID.sh
+++ summative/backup-Negpod_ID.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+# Remote server details
 remote_host="a0dfc11ba139.a982e858.alu-cod.online"
 remote_user="a0dfc11ba139"
 remote_password="899c79fc532cd0591af1"
 remote_location="/home/sftp-student/03033/summative"
 
+# Directory to be backed up
 directory_to_backup="negpod_id-q1"
 
+# Archive the directory to a tar.gz file
 tar_filename="${directory_to_backup}.tar.gz"
 tar -czf "$tar_filename" "$directory_to_backup"
 
+# Transfer the tar.gz file to the remote server using sftp
 echo "put $tar_filename" | sftp "$remote_user@$remote_host:$remote_location" <<EOF
 $remote_password
 EOF
 
+# Remove the local tar.gz file after backup
 rm "$tar_filename"
 
 echo "Backup completed to the remote server."
--- summative/negpod_id-q1/main.sh
+++ summative/negpod_id-q1/main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# The Function to create a student record and save it in the file
 create_student_record() {
     echo "Enter student email:"
     read email
@@ -14,43 +15,50 @@ create_student_record() {
     echo "Student record created successfully!"
 }
 
+# Function to view all students in the file
 view_all_students() {
     echo "List of students:"
     cat students-list_0333.txt
 }
 
+# Function to delete a student record by student ID
 delete_student() {
     echo "Enter the student ID to delete:"
     read student_id
 
+    # Create a temporary file without the student to be deleted
     grep -v "$student_id" students-list_0333.txt > temp.txt
     mv temp.txt students-list_0333.txt
 
     echo "Student with ID $student_id deleted successfully!"
 }
 
+# Function to update a student record by student ID
 update_student() {
     echo "Enter the student ID to update:"
     read student_id
 
+    # Create a temporary file to store the updated student record
     echo "Enter updated student email:"
     read email
 
     echo "Enter updated student age:"
     read age
 
+    # Find the student by ID and update the record
     awk -v sid="$student_id" -v em="$email" -v ag="$age" 'BEGIN{FS=OFS=","} $3==sid{$1=em; $2=ag}1' students-list_0333.txt > temp.txt
     mv temp.txt students-list_0333.txt
 
     echo "Student with ID $student_id updated successfully!"
 }
 
+# Function to exit the application
 exit_app() {
     echo "Exiting the application."
     exit 0
 }
 
-# Main menu
+# All options to make
 while true; do
     echo "Choose an option:"
     echo "1. Create student record"
