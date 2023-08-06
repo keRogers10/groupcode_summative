6d01b5c603f0fe9b91b63b568b223ce0aaf61a8c
ChukwukaJ <c.nwachukwu1@alustudent.com>
2023-08-06 09:30:19 -0700

    newstuff

--- summative/move-to-directory.sh
+++ summative/move-to-directory.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Create  the negpod_id-q1 directory if not there
-mkdir -p negpod_id-q1
+# Create  the negpod_1-q1 directory if not there
+mkdir -p negpod_1-q1
 
-# Move this thing of a files to the negpod_id-q1 directory
-mv students-list_0333.txt student-emails.txt main.sh select-emails.sh negpod_id-q1
+# Move this thing of a files to the negpod_1-q1 directory
+mv students-list_0333.txt student-emails.txt main.sh select-emails.sh negpod_1-q1
 
-echo "Files moved to the negpod_id-q1 directory."
+echo "Files moved to the negpod_1-q1 directory."
 
